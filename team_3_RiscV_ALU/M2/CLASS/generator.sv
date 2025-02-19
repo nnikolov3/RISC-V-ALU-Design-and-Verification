1439c217b9c488509881436ed3c388cf54358f0d
AMaldonado-liu <maldon7@pdx.edu>
2025-02-18 20:57:12 -0800

    Update generator.sv
    added a deactivate task

--- team_3_RiscV_ALU/M2/CLASS/generator.sv
+++ team_3_RiscV_ALU/M2/CLASS/generator.sv
@@ -72,6 +72,8 @@ class generator;
 
         // Generate scenarios with random values and varying constraints.
         generate_random_scenarios();
+		
+		deactivate_ce();
 
         // Signal that the generation process is complete.
         ->generation_complete;
@@ -346,6 +348,15 @@ class generator;
         gen2drv_mb.put(trans.clone());
         print_scenario("FENCE: Memory Ordering");
     endtask
+	
+	
+	task deactivate_ce();
+		
+		trans.set_values(0, `FENCE_BITS, 32'h0, 32'h0, 32'h0, 1'b0);
+        gen2drv_mb.put(trans.clone());
+        print_scenario("Deactivate Clock Enable");
+		
+	endtask
 
     //-------------------------------------------------------------------------
     // Function: print_scenario
